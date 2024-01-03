4b495bd4b55782117b4a851fbdf6dab105d9724e
eli-eliyahu <105585737+eli-eliyau@users.noreply.github.com>
2024-01-03 10:14:26 +0200

    Delete server/dist directory

--- server/dist/routers/rMessages.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const cMessages_1 = require("../controller/cMessages");
-const router = (0, express_1.Router)();
-router.post("/setMessage", cMessages_1.setMessage);
-exports.default = router;
--- server/dist/routers/rSignIn.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const cUsers_1 = require("../controller/cUsers");
-const router = (0, express_1.Router)();
-router.post("/signIn", cUsers_1.getUser);
-exports.default = router;
